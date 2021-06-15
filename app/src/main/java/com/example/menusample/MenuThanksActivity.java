632008df869cc9d7bc3e2571e121a237439b59cf
KoshiDra <koshidradev@gmail.com>
2021-06-15 19:27:45 +0900

    アクションバーに戻るメニューを表示

--- app/src/main/java/com/example/menusample/MenuThanksActivity.java
+++ app/src/main/java/com/example/menusample/MenuThanksActivity.java
@@ -1,9 +1,11 @@
 package com.example.menusample;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,10 +25,34 @@ public class MenuThanksActivity extends AppCompatActivity {
 
         TextView tv2 = findViewById(R.id.tvMenuPrice);
         tv2.setText(price);
+
+        // -- アクションバーに「戻る」メニューを表示
+        // アクションバーを取得
+        ActionBar actionBar = getSupportActionBar();
+
+        // アクションバーの「戻る」メニューを有効化
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    public void onBackButtonClick(View v) {
-        // 戻るボタン押下（アクティビティの終了）
-        finish();
+    /**
+     * 「戻る」メニュー選択時の処理
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        boolean retval = true;
+
+        // 選択されたメニューIDを取得
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            // 選択されたメニューが「戻る」の場合はアクティビティを終了
+            finish();
+        } else {
+            retval = super.onOptionsItemSelected(item);
+        }
+
+        return retval;
     }
 }
